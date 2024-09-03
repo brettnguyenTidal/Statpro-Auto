2ecc153a2226b7e1133dfc45c4db26a56768bf75
Brett Nguyen <bnguyen@tidalfg.com>
2024-09-03 00:27:02 -0400

    fix

--- auto_download.py
+++ auto_download.py
@@ -27,9 +27,9 @@ def convert_to_number(value):
 
 # Set up headless mode for GitHub Actions environment
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 # Set download path within the repository
 download_path = os.path.join(os.getcwd(), "downloads")
